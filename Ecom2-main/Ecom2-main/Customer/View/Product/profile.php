2aa0fb67406fdc35ecacc4947302b14bc4da5044
Drey <bhon3myatpaing@gmail.com>
2023-07-21 00:38:19 +0630

    profile

---
+++ Ecom2-main/Ecom2-main/Customer/View/Product/profile.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+    <link rel="stylesheet" href="../resources/lib/tailwind/output.css">
+</head>
+<body class="bg-slate-100">
+    <!-- nav bar -->
+    <div class="flex justify-between items-center">
+        <div class="">
+            <svg width="173" height="103" viewBox="0 0 173 103" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g filter="url(#filter0_d_600_22)">
+                    <path d="M22 16H26.56L34.8 23.76V29.8L27.04 22.48V39H22V16ZM49.56 16H45.04L36.76 23.76V29.8L44.52 22.48V39H49.56V16ZM57.5469 16H70.3469V20.72H62.5869V25.24H70.3469V29.8H62.5869V34.48H70.1869V39H57.5469V16ZM72.3069 34.48V39H85.1069V34.48H72.3069ZM85.1069 20.72V16H72.1469V20.72H85.1069ZM78.6269 29.8V25.24H72.1469V29.8H78.6269ZM89.5203 18.76V36.28L94.2003 39H102.32V34.48H94.7203V20.72H102.48V16H94.2003L89.5203 18.76ZM117.24 23.44H112.04V20.72H104.28V16H112.56L117.24 18.76V23.44ZM117.24 25.24V36.28L112.56 39H104.44V34.48H112.04V29.8H104.28V25.24H117.24ZM123.622 18.76V39H128.662V29.8H136.422V25.24H128.662V20.52H136.422V16H128.182L123.622 18.76ZM151.182 18.76V39H146.142V29.8H138.222V25.24H146.142V20.52H138.222V16H146.462L151.182 18.76ZM20.8 58.76V67.2L25.36 69.92H33.6V65.24H25.68V60.72H33.6V56H25.36L20.8 58.76ZM48.36 68V76.28L43.64 79H35.56V74.48H43.32V69.92H35.4V65.24H43.64L48.36 68ZM25.84 71.72H20.8V76.28L25.48 79H33.6V74.48H25.84V71.72ZM48.36 63.44H43.32V60.72H35.4V56H43.64L48.36 58.76V63.44ZM55.1641 56H60.2041V65.24H67.9641V69.8H60.2041V79H55.1641V56ZM82.7241 56H77.6841V65.24H69.9241V69.8H77.6841V79H82.7241V56ZM89.9109 58.76L94.5909 56H102.871V60.52H94.9509V74.32H102.711V79H94.5909L89.9109 76.28V58.76ZM117.471 58.76L112.951 56H104.671V60.52H112.431V74.32H104.671V79H112.951L117.471 76.28V58.76ZM124.656 56H137.616V60.52H129.856V65.24H137.616V69.92H129.856V79H124.656V56ZM152.376 67.2V58.76L147.696 56H139.416V60.52H147.176V65.24H139.416V69.92H147.696L152.376 67.2Z" fill="#003366" />
+                </g>
+                <defs>
+                    <filter id="filter0_d_600_22" x="0.799805" y="0" width="171.576" height="103" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                        <feOffset dy="4" />
+                        <feGaussianBlur stdDeviation="10" />
+                        <feComposite in2="hardAlpha" operator="out" />
+                        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_600_22" />
+                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_600_22" result="shape" />
+                    </filter>
+                </defs>
+            </svg>
+        </div>
+
+        <div class="flex justify-center items-center ">
+            <p class="py-3 px-5 font-bold border-4 border-transparent hover:border-b-blue-800 cursor-pointer">HOME</p>
+            <p class="py-3 px-5 font-bold border-4 border-transparent hover:border-b-blue-800 cursor-pointer">PRODUCTS</p>
+            <p class="py-3 px-5 font-bold border-4 border-transparent hover:border-b-blue-800 cursor-pointer">STORES</p>
+            <p class="py-3 px-5 font-bold border-4 border-transparent hover:border-b-blue-800 cursor-pointer">ABOUT US</p>
+            <p class="py-3 px-5 font-bold border-4 border-transparent hover:border-b-blue-800 cursor-pointer">CONTACT US</p>
+        </div>
+        <div class="flex justify-between items-center">
+            <div class="px-5">
+                <svg width="65" height="55" viewBox="0 0 65 55" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M36.8 47.2C34.247 47.2 32.2 48.9355 32.2 51.1C32.2 52.1343 32.6846 53.1263 33.5473 53.8577C34.41 54.5891 35.58 55 36.8 55C38.02 55 39.19 54.5891 40.0527 53.8577C40.9154 53.1263 41.4 52.1343 41.4 51.1C41.4 50.0657 40.9154 49.0737 40.0527 48.3423C39.19 47.6109 38.02 47.2 36.8 47.2ZM0 16V19.9H4.6L12.88 34.7005L9.752 39.478C9.407 40.024 9.2 40.6675 9.2 41.35C9.2 42.3843 9.68464 43.3763 10.5473 44.1077C11.41 44.8391 12.58 45.25 13.8 45.25H41.4V41.35H14.766C14.6135 41.35 14.4672 41.2986 14.3594 41.2072C14.2516 41.1158 14.191 40.9918 14.191 40.8625C14.191 40.765 14.214 40.687 14.26 40.6285L16.33 37.45H33.465C35.19 37.45 36.708 36.631 37.49 35.4415L45.724 22.825C45.885 22.513 46 22.1815 46 21.85C46 21.3328 45.7577 20.8368 45.3263 20.4711C44.895 20.1054 44.31 19.9 43.7 19.9H9.683L7.521 16M13.8 47.2C11.247 47.2 9.2 48.9355 9.2 51.1C9.2 52.1343 9.68464 53.1263 10.5473 53.8577C11.41 54.5891 12.58 55 13.8 55C15.02 55 16.19 54.5891 17.0527 53.8577C17.9154 53.1263 18.4 52.1343 18.4 51.1C18.4 50.0657 17.9154 49.0737 17.0527 48.3423C16.19 47.6109 15.02 47.2 13.8 47.2Z" fill="#024486" />
+                    <rect x="46" width="19" height="19" rx="9.5" fill="#F68721" />
+                    <path d="M54.777 14.0994C54.2775 14.0994 53.8454 14.0071 53.4808 13.8224C53.1186 13.6354 52.8499 13.3797 52.6747 13.0554C52.4995 12.7311 52.4415 12.3606 52.5007 11.9439H53.9922C53.9638 12.2232 54.0443 12.4517 54.2337 12.6293C54.4231 12.8045 54.6705 12.892 54.9759 12.892C55.2173 12.892 55.4399 12.8388 55.6435 12.7322C55.8471 12.6233 56.0175 12.473 56.1548 12.2812C56.2945 12.0895 56.3845 11.867 56.4247 11.6136C56.4673 11.358 56.4508 11.133 56.375 10.9389C56.2992 10.7448 56.1761 10.5933 56.0057 10.4844C55.8352 10.3731 55.6269 10.3175 55.3807 10.3175C55.1652 10.3175 54.951 10.3625 54.7379 10.4524C54.5249 10.54 54.3497 10.6584 54.2124 10.8075L52.8842 10.5625L53.8679 6.72727H58.3139L58.108 7.98438H54.9332L54.4396 9.80611H54.4822C54.6409 9.62618 54.8587 9.47704 55.1357 9.35866C55.4126 9.24029 55.7109 9.18111 56.0305 9.18111C56.4685 9.18111 56.8414 9.28409 57.1491 9.49006C57.4593 9.69602 57.683 9.97893 57.8203 10.3388C57.9576 10.6986 57.9872 11.1106 57.9091 11.5746C57.831 12.0646 57.6475 12.4991 57.3587 12.8778C57.0698 13.2566 56.7029 13.5549 56.2578 13.7727C55.8127 13.9905 55.3191 14.0994 54.777 14.0994Z" fill="white" />
+                </svg>
+            </div>
+            <div class="px-5">
+                <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M22.8824 1.56689C11.0449 1.56689 1.44922 11.1625 1.44922 23.0001C1.44922 34.8377 11.0449 44.4333 22.8824 44.4333C34.72 44.4333 44.3156 34.8377 44.3156 23.0001C44.3156 11.1625 34.72 1.56689 22.8824 1.56689Z" stroke="#024486" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M6.31738 36.6016C6.31738 36.6016 11.0948 30.5017 22.8831 30.5017C34.6714 30.5017 39.451 36.6016 39.451 36.6016M22.8831 23.0001C24.5884 23.0001 26.2239 22.3226 27.4298 21.1168C28.6356 19.9109 29.3131 18.2754 29.3131 16.5701C29.3131 14.8648 28.6356 13.2293 27.4298 12.0234C26.2239 10.8176 24.5884 10.1401 22.8831 10.1401C21.1778 10.1401 19.5423 10.8176 18.3364 12.0234C17.1306 13.2293 16.4531 14.8648 16.4531 16.5701C16.4531 18.2754 17.1306 19.9109 18.3364 21.1168C19.5423 22.3226 21.1778 23.0001 22.8831 23.0001Z" stroke="#024486" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+            </div>
+
+        </div>
+    </div>
+    <div class="w-full h-1 bg-blue-800"></div>
+    <!-- body -->
+    <div class="flex justify-between items-center">
+        <div class="w-1/6 h-1/2 mx-auto bg-gray-100 text-center rounded-xl py-4 mt-36 shadow-2xl">
+
+            <p class="text-xl font-bold py-3">User Profile</p>
+
+            <div class="w-full h-1 bg-black"></div>
+
+            <p class="text-md py-3 font-semibold cursor-pointer">User Info</p>
+
+
+            <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">Followed Stores</p>
+
+
+            <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">Order History</p>
+
+            <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">My Reviews</p>
+
+            <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">Wishlist</p>
+
+            <div class="flex justify-center items-center">
+                <ion-icon name="log-out-outline" class="text-xl font-light cursor-pointer hover:font-semibold"></ion-icon>
+                <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">Logout</p>
+
+            </div>
+        </div>
+        <div class="w-1/2 h-1/2 mx-auto bg-gray-200 text-center rounded-xl py-4 m-l-2">
+            <div class="flex  items-center justify-around">
+                <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-1/6 h-1/6">
+                    <path d="M22.8824 1.56689C11.0449 1.56689 1.44922 11.1625 1.44922 23.0001C1.44922 34.8377 11.0449 44.4333 22.8824 44.4333C34.72 44.4333 44.3156 34.8377 44.3156 23.0001C44.3156 11.1625 34.72 1.56689 22.8824 1.56689Z" stroke="#024486" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M6.31738 36.6016C6.31738 36.6016 11.0948 30.5017 22.8831 30.5017C34.6714 30.5017 39.451 36.6016 39.451 36.6016M22.8831 23.0001C24.5884 23.0001 26.2239 22.3226 27.4298 21.1168C28.6356 19.9109 29.3131 18.2754 29.3131 16.5701C29.3131 14.8648 28.6356 13.2293 27.4298 12.0234C26.2239 10.8176 24.5884 10.1401 22.8831 10.1401C21.1778 10.1401 19.5423 10.8176 18.3364 12.0234C17.1306 13.2293 16.4531 14.8648 16.4531 16.5701C16.4531 18.2754 17.1306 19.9109 18.3364 21.1168C19.5423 22.3226 21.1778 23.0001 22.8831 23.0001Z" stroke="#024486" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+                <div class="flex flex-col items-center">
+                    <p class="text-xl font-bold py-6">David</p>
+                    <p class="text-lg font-semibold py-6">Yangon, Myanmar</p>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
