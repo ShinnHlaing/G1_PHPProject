9d7c1f4fa295da0690b4806d93545e3e52fff515
Hein Thu <88365143+hanehein@users.noreply.github.com>
2023-07-20 22:29:39 +0630

    project-frame

---
+++ Ecom2-main/Ecom2-main/Customer/View/Product/test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Item Detail</title>
+    <link rel="stylesheet" href="../resources/lib/tailwind/output.css">
+    <link rel="stylesheet" href="../resources/css/itemDetail.css">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</head>
+
+<body class="text-lg bg-red-300">
+    <h1>Testing Page</h1>
+</body>
+
+</html>
