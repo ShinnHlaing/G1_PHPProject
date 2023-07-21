79e8ba9e2a8bac31ee34b264c5d61017d3e44c63
Drey <bhon3myatpaing@gmail.com>
2023-07-21 10:07:16 +0630

    profile-update

--- Ecom2-main/Ecom2-main/Customer/View/Product/profile.php
+++ Ecom2-main/Ecom2-main/Customer/View/Product/profile.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Profile</title>
     <link rel="stylesheet" href="../resources/lib/tailwind/output.css">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body class="bg-slate-100">
     <!-- nav bar -->
     <div class="flex justify-between items-center">
@@ -62,11 +67,9 @@
 
             <div class="w-full h-1 bg-black"></div>
 
-            <p class="text-md py-3 font-semibold cursor-pointer">User Info</p>
-
+            <p class="text-md py-3 font-semibold hover:font-semibold cursor-pointer">User Info</p>
 
             <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">Followed Stores</p>
-
 
             <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">Order History</p>
 
@@ -80,8 +83,8 @@
 
             </div>
         </div>
-        <div class="w-1/2 h-1/2 mx-auto bg-gray-200 text-center rounded-xl py-4 m-l-2">
-            <div class="flex  items-center justify-around">
+        <div class="w-5/12 h-1/2 mx-auto bg-gray-100 shadow-2xl text-center rounded-xl py-4 mt-5">
+            <div class="flex  items-center justify-evenly">
                 <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-1/6 h-1/6">
                     <path d="M22.8824 1.56689C11.0449 1.56689 1.44922 11.1625 1.44922 23.0001C1.44922 34.8377 11.0449 44.4333 22.8824 44.4333C34.72 44.4333 44.3156 34.8377 44.3156 23.0001C44.3156 11.1625 34.72 1.56689 22.8824 1.56689Z" stroke="#024486" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                     <path d="M6.31738 36.6016C6.31738 36.6016 11.0948 30.5017 22.8831 30.5017C34.6714 30.5017 39.451 36.6016 39.451 36.6016M22.8831 23.0001C24.5884 23.0001 26.2239 22.3226 27.4298 21.1168C28.6356 19.9109 29.3131 18.2754 29.3131 16.5701C29.3131 14.8648 28.6356 13.2293 27.4298 12.0234C26.2239 10.8176 24.5884 10.1401 22.8831 10.1401C21.1778 10.1401 19.5423 10.8176 18.3364 12.0234C17.1306 13.2293 16.4531 14.8648 16.4531 16.5701C16.4531 18.2754 17.1306 19.9109 18.3364 21.1168C19.5423 22.3226 21.1778 23.0001 22.8831 23.0001Z" stroke="#024486" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
@@ -91,10 +94,39 @@
                     <p class="text-lg font-semibold py-6">Yangon, Myanmar</p>
                 </div>
             </div>
-            <div>
 
+            <div class="flex flex-col mx-auto">
+                <div class="w-1/2 ml-24 mt-3 ">
+                    <p class="text-xl font-semibold">Name</p>
+                </div>
+                <div class="mt-5 cursor-pointer">
+                    <input type="text" name="name" id="" class="w-1/3 h-10 rounded-md border-solid border-2 border-black px-5">
+                </div>
+                <div class="w-1/2 ml-32 mt-5">
+                    <p class="text-xl font-semibold">Email address</p>
+                </div>
+                <div class="mt-5 cursor-pointer">
+                    <input type="email" name="name" id="" class="w-1/3 h-10 rounded-md border-solid border-2 border-black px-5">
+                </div>
+                <div class="w-1/2 ml-28 mt-5">
+                    <p class="text-xl font-semibold">Location</p>
+                </div>
+                <div class="mt-5 cursor-pointer">
+                    <input type="text" name="name" id="" class="w-1/3 h-10 rounded-md border-solid border-2 border-black px-5">
+                </div>
+                <div class="w-1/2 ml-36 mt-5">
+                    <p class="text-xl font-semibold">Phone Number</p>
+                </div>
+                <div class="mt-5 cursor-pointer">
+                    <input type="number" inputmode="numeric" name="name" id="" class="w-1/3 h-10 rounded-md border-solid border-2 border-black px-5">
+                </div>
+
+            </div>  
+            <div class="bg-orange-500 w-1/12 mt-10 py-2 rounded-lg mr-64 mb-20 float-right cursor-pointer">
+                <p class="text-white font-bold">Edit</p>
             </div>
         </div>
     </div>
 </body>
+
 </html>
